274c11db0725f0432cafffd56a25c4d3c4d2b1d5
GrooveypenguinX <grooveypenguinx@gmail.com>
2026-09-13 12:13:27 -0400

    added back armory compat

--- EcoWW2Pack/EcoWW2Pack.cs
+++ EcoWW2Pack/EcoWW2Pack.cs
@@ -1,10 +1,13 @@
-﻿using System.Reflection;
+﻿using EcoWW2Pack.Helpers;
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Common;
 using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Core.Models.Spt.Tables;
+using System.Reflection;
+using WTTServerCommonLib.Helpers;
 using WTTServerCommonLib.Models;
 using Range = SemanticVersioning.Range;
-using EcoWW2Pack.Helpers;
 
 namespace EcoWW2Pack;
 
@@ -39,5 +42,57 @@ public class EcoWW2Pack(
         await wttCommon.CustomBotLoadoutService.CreateCustomBotLoadouts(assembly);
         await wttCommon.CustomAssortSchemeService.CreateCustomAssortSchemes(assembly);
         ecoQuestHelper.ModifyQuests();
+    }
+}
+
+[Injectable(InjectionType.Singleton, TypePriority = OnLoadOrder.PostLoad + 2)]
+public class EcoWW2PackPostSptLoad(
+    IReadOnlyList<SptMod> modList,
+    TemplateTable templateTable,
+    SlotHelper slotHelper) : IOnLoad
+{
+    private static readonly string[] ArmoryAmmoIds =
+    [
+        "66d0c1319aba7b6bff460a37",
+        "682e8c4bc098b59f14dde097",
+        "6861847dfd5210a3e64677a3"
+    ];
+
+    public Task OnLoadAsync(CancellationToken cancellationToken)
+    {
+        if (!modList.Any(mod => mod.ModMetadata.ModGuid == "com.wtt.armory"))
+        {
+            return Task.CompletedTask;
+        }
+
+        var items = templateTable.Items;
+
+        if (items.TryGetValue("6a0ce40499dfd9edaca1daa9", out var kar98))
+        {
+            AddAmmoToFilter(kar98.Properties.Chambers.FirstOrDefault()?.Properties.Filters.FirstOrDefault()?.Filter);
+        }
+
+        if (items.TryGetValue("6a0ced4599dfd9edaca1daaa", out var kar98mag))
+        {
+            AddAmmoToFilter(kar98mag.Properties.Cartridges.FirstOrDefault()?.Properties.Filters.FirstOrDefault()?.Filter);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private static void AddAmmoToFilter(HashSet<MongoId>? filter)
+    {
+        if (filter == null)
+        {
+            return;
+        }
+
+        foreach (var ammoId in ArmoryAmmoIds)
+        {
+            if (!filter.Contains(ammoId))
+            {
+                filter.Add(ammoId);
+            }
+        }
     }
 }
